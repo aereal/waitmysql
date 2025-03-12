d9e62f099dcc39f8408ea48cfb630e4f129d1bb7
aereal <trasty.loose@gmail.com>
2025-03-12 18:52:27 +0900

    fix: put implementations into internal/cli package

--- cmd/waitmysql/main.go
+++ cmd/waitmysql/main.go
@@ -2,73 +2,12 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"errors"
-	"flag"
-	"fmt"
 	"os"
-	"time"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/shogo82148/go-retry"
+	"github.com/aereal/waitmysql/internal/cli"
 )
 
 func main() {
-	os.Exit(int(run(os.Args)))
-}
-
-type statusCode int
-
-const (
-	statusOK statusCode = iota
-	statusError
-)
-
-func run(argv []string) statusCode {
-	flagset := flag.NewFlagSet(argv[0], flag.ContinueOnError)
-	var (
-		dsn         string
-		initialWait time.Duration
-		maxDelay    time.Duration
-		maxAttempts int
-	)
-	flagset.StringVar(&dsn, "dsn", "", "data source name string")
-	flagset.DurationVar(&initialWait, "init-wait", 0, "initial delay")
-	flagset.DurationVar(&maxDelay, "max-delay", -1, "max delay (default is infinity)")
-	flagset.IntVar(&maxAttempts, "max-attempts", -1, "max attempts count")
-	if err := flagset.Parse(argv[1:]); err != nil {
-		if errors.Is(err, flag.ErrHelp) {
-			return statusOK
-		}
-		fmt.Printf("%+v\n", err)
-		return statusError
-	}
-	if dsn == "" {
-		fmt.Println("-dsn must be given")
-		return statusError
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	policy := &retry.Policy{
-		MinDelay: initialWait,
-		MaxDelay: maxDelay,
-		MaxCount: maxAttempts,
-	}
-	if err := policy.Do(ctx, func() error { return checkConnection(ctx, dsn) }); err != nil {
-		fmt.Printf("%+v\n", err)
-		return statusError
-	}
-	return statusOK
-}
-
-func checkConnection(ctx context.Context, dsn string) error {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return retry.MarkPermanent(err)
-	}
-	if err := db.PingContext(ctx); err != nil {
-		return err
-	}
-	return nil
+	app := cli.New(os.Stdin, os.Stdout, os.Stderr)
+	os.Exit(app.Run(context.Background(), os.Args))
 }
